287e5bb8608a83335e0226746463153baf742bac
tww178 <tinwai.a.wong@gmail.com>
2025-11-04 23:01:30 +0000

    CH2 L5 before looking at solution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pygame
+import pygame 
+import circleshape
+import player
 from constants import *
 
 def main():
@@ -6,12 +8,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+    player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
     print("Starting Asteroids!")
