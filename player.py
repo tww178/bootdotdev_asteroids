7fd7a0ccc217204725f8f483f80c6ef1f6d34f8b
tww178 <tinwai.a.wong@gmail.com>
2025-11-05 16:01:58 +0000

    CH3 L4 checked answer, slightly different but runnable

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0 #No idea why this timer works
 
     # in the player class
     def triangle(self):
@@ -30,10 +31,15 @@ class Player(CircleShape):
     def shoot(self):
         #wrong code: shot = Shot(self.x, self.y, SHOT_RADIUS)
         #redundant: shot.position = self.position
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED #Why doesn't this need dt?
+        self.timer = PLAYER_SHOOT_COOLDOWN #This line is right after return in the answer
+
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
